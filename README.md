27e5dec24e8e42833a664d3774e6af2aa187ff03
Joelimgu <joelimgu@gmail.com>
2021-06-25 23:08:38 +0200

    stuff

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 install youtube-dl: pip3 install youtube-dl
+
+the nightly verson of rust is needed
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,84 @@
+// mod tui;
+// extern crate reqwest;
+// use std::env;
+// use std::fs;
+// use std::path::PathBuf;
+// use futures::TryFutureExt;
+// use futures::executor;
+// use youtube_dl::YoutubeDl;
 
-extern crate reqwest;
-use std::env;
-use std::fs;
-use youtube_dl::YoutubeDl;
+// async fn req(key: &str) -> Result<String, reqwest::Error> {
+//     //let client = reqwest::Client::new();
+//     let search_keyword = "ben awad";
+//     let url = format!("{}{}{}{}", "https://www.googleapis.com/youtube/v3/search?&maxResults=25&q=\"",  search_keyword.clone(), "\"&key=", &key[1..key.chars().count() - 1]);
+//     let res = reqwest::get(&url).await?;
+
+//     println!("Status: {}", res.status());
+//     println!("Headers:\n{:#?}", res.headers());
+
+//     // Move and borrow value of `res`
+//     let body = res.text().await?;
+//     println!("Body:n{}", body);
+//     println!("url used:{}", url);
+
+//     Ok(body)
+// }
 
 
-#[tokio::main]
-async fn req(key: &str) -> Result<String, reqwest::Error> {
-    //let client = reqwest::Client::new();
-    let search_keyword = "ben awad";
-    let url = format!("{}{}{}{}", "https://www.googleapis.com/youtube/v3/search?&maxResults=25&q=\"",  search_keyword.clone(), "\"&key=", &key[1..key.chars().count() - 1]);
-    let res = reqwest::get(&url).await?;
+// async fn download(){
 
-    println!("Status: {}", res.status());
-    println!("Headers:\n{:#?}", res.headers());
+//     let url = "https://www.youtube.com/watch?v=Edx9D2yaOGs&ab_channel=CollegeHumor";
+//     let path_to_video = rustube::download_best_quality(url).await;
+    
 
-    // Move and borrow value of `res`
-    let body = res.text().await?;
-    println!("Body:n{}", body);
-    println!("url used:{}", url);
+//     print!("path:{:?}", path_to_video);
+//     println!("HERE")
+// }
 
-    Ok(body)
-}
+//#[tokio::main(flavor = "current_thread")]
+// fn main() {
+//     /*
+//     let contents = fs::read_to_string("/home/joel/Documents/Code/youtube-tui/src/config.json")
+//         .expect("Something went wrong reading the file");
+//     println!("With text:\n{}", contents);
+    
+//     let json: serde_json::Value = serde_json::from_str(&contents).expect("JSON was not well-formatted");
+//     println!("JSON:{}", json["key"]);
+//     let res: serde_json::Value = serde_json::from_str(&req(&json["key"].to_string()).unwrap())
+//         .expect("JSON was not well-formatted");
+    
+    
+//     let output = YoutubeDl::new("https://www.youtube.com/watch?v=VFbhKZFzbzk")
+//         .socket_timeout("15")
+//         .run()
+//         .unwrap();
+
+//     let output = match output {
+//         youtube_dl::YoutubeDlOutput::Playlist(_) => {Err("playlists not supported yet")},
+//         youtube_dl::YoutubeDlOutput::SingleVideo(res) => {Ok(*res)}
+//     };
+
+//     println!("test:{:?}", output.unwrap().downloader_options);
+//     */
+//     // let handle = tokio::runtime::Handle::current();
+//     // handle.enter();
+
+//     //download().await;
+//     let url = "https://youtu.be/nv2wQvn6Wxc";
+//     let path_to_video = rustube::blocking::download_best_quality(url).unwrap();
+//     println!("{:?}", path_to_video)
+// }
+
+// working!!
+// #[rustube::tokio::main]
+// async fn main() {
+//     let url = "https://www.youtube.com/watch?v=Edx9D2yaOGs&ab_channel=CollegeHumor";
+//     println!("downloaded video to {:?}", rustube::download_best_quality(&url).await.unwrap());
+// }
+
+
+mod tui;
 
 fn main(){
-    let contents = fs::read_to_string("/home/joel/Documents/Code/youtube-tui/src/config.json")
-        .expect("Something went wrong reading the file");
-    println!("With text:\n{}", contents);
-    
-    let json: serde_json::Value = serde_json::from_str(&contents).expect("JSON was not well-formatted");
-    println!("JSON:{}", json["key"]);
-    let res: serde_json::Value = serde_json::from_str(&req(&json["key"].to_string()).unwrap())
-        .expect("JSON was not well-formatted");
-    
-    let output = YoutubeDl::new("https://www.youtube.com/watch?v=VFbhKZFzbzk")
-        .socket_timeout("15")
-        .run()
-        .unwrap();
-    println!("codec:{}", output.to_single_video());
+    let _y = tui::render();
 }
